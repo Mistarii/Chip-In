7642338b24eb833e0856783ec785ebe656c2a04e
Daemin Lee <dmnlee95@gmail.com>
2018-09-14 14:04:35 -0400

    Fixed Errors for Accounts and API

--- ChipIn/app/src/main/java/com/example/mistarii/chip_in/RestFiles/AccountManagementAPI.java
+++ ChipIn/app/src/main/java/com/example/mistarii/chip_in/RestFiles/AccountManagementAPI.java
@@ -11,37 +11,34 @@ import java.net.URL;
 //import java.net.HttpURLConnection*;
 
 public class AccountManagementAPI {
-    private string apiKey = "QsypVK0SKMd1WRqVy9Rc";
-    public string accounts = "/accounts";
-    private string targetAccount;
-
-
-
-
-    //calls the create account REST call to API
-    string GetAccounts() throws MalformedURLException {
-        try{
-        URL getAccountsURL = new URL("https://us-central1-incomm-hackathon-api.cloudfunctions.net/api/accounts");
-        HttpURLConnection getAccountconnection = (HttpURLConnection) getAccountsURL.openConnection();
-        getAccountconnection.setRequestMethod("GET");
-
-        if (getAccountconnection.getResponseCode() != 200){
-                throw new RuntimeException("Failed Https error code: " + getAccountconnection.getResponseCode());
-            }
-    }
-    catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //calls the get account REST call to api
-    //void getAccounts(){
-
-        return baseURL;
-    }
-
-    void deleteAccount() {
-
-    }
+//    String apiKey = "QsypVK0SKMd1WRqVy9Rc";
+//    String accounts = "/accounts";
+//    String targetAccount;
+//
+//    //calls the create account REST call to API
+//    string GetAccounts() throws MalformedURLException {
+//        try{
+//        URL getAccountsURL = new URL("https://us-central1-incomm-hackathon-api.cloudfunctions.net/api/accounts");
+//        HttpURLConnection getAccountconnection = (HttpURLConnection) getAccountsURL.openConnection();
+//        getAccountconnection.setRequestMethod("GET");
+//
+//        if (getAccountconnection.getResponseCode() != 200){
+//                throw new RuntimeException("Failed Https error code: " + getAccountconnection.getResponseCode());
+//            }
+//    }
+//    catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //calls the get account REST call to api
+//    //void getAccounts(){
+//
+//        return baseURL;
+//    }
+//
+//    void deleteAccount() {
+//
+//    }
 
 
 }
